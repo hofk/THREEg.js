3aeb6fa4592bdcc9f219e5db63d8fe059e34123c
Klaus Hoffmeister <klaus@hofk.de>
2019-01-19 20:36:59 +0100

    Add files via upload

--- THREEg.js
+++ THREEg.js
@@ -1,4 +1,4 @@
-// THREEg.js ( rev 99.0 )
+// THREEg.js ( rev 100.0 )
 
 /**
  * @author hofk / http://threejs.hofk.de/
@@ -3181,6 +3181,283 @@ function buildLineGrid( ) {
 
 exports.createLineGrid = createLineGrid;
 exports.buildLineGrid = buildLineGrid;
+
+// .............................  ProfiledContourGeometryMM  .........................................
+
+function createProfiledContourMMgeometry( profileShape, contour, contourClosed, openEnded, profileMaterial ) {
+											// the last three parameters are optional	
+	g = this;  //  THREE.BufferGeometry() - geometry object from three.js
+			
+	g.profileShape = profileShape;
+	g.contour = contour;
+
+	g.contourClosed = contourClosed !== undefined ? contourClosed : true;
+	g.openEnded = openEnded !== undefined ? openEnded : false;
+	g.openEnded = contourClosed === true ? false : openEnded;
+	g.profileMaterial = profileMaterial !== undefined ? profileMaterial : false;
+		
+	if( g.contourClosed ) g.contour.push( g.contour[ 0 ], g.contour[ 1 ] );
+	
+	g.buildProfiledContourMMgeometry = buildProfiledContourMMgeometry;
+	g.buildProfiledContourMMgeometry();
+	
+}
+	
+function buildProfiledContourMMgeometry( ) {
+			
+	var hs1 = g.contour.length / 2;
+	var rs1 = g.profileShape.length / 2;
+	var hs = hs1 - 1; // height segments 
+	var rs = rs1 - 1; // radius segments
+	
+	var faceCount = hs * rs * 2 + ( g.openEnded ? 0 : rs * 2 );
+	var posCount = hs1 * rs1 + ( g.openEnded ? 0 : 2 );
+	
+	g.indices = new Uint32Array( faceCount * 3 );  // indexed BufferGeometry
+	g.positions = new Float32Array( posCount * 3 );
+	
+	g.setIndex( new THREE.BufferAttribute( g.indices, 1 ) );
+	g.addAttribute( 'position', new THREE.BufferAttribute( g.positions, 3 ) );
+	
+	var a, b1, c1, b2, c2;
+	var i1, i2;
+	var xc0, yc0, xc1, yc1, xc2, yc2, xSh, xDiv;
+	var dx0, dy0, dx2, dy2;
+	var e0x, e0y,e0Length, e2x, e2y, e2Length, ex, ey, eLength;
+	var phi, bend;
+	var x, y, z;
+	var vIdx, posIdx;
+	var epsilon = 0.000001;
+	var idx = 0;
+	
+	for ( var i = 0; i < hs; i ++ ) {
+		
+		if ( g.profileMaterial ) g.addGroup( idx, rs * 6, i ); // MultiMaterial support
+		
+		i1 = i + 1;
+		
+		for ( var j = 0; j < rs; j ++ ) {
+			
+			// 2 faces / segment,  3 vertex indices
+			a =  rs1 * i + j;
+			c1 = rs1 * i1 + j; // left 
+			b1 = c1 + 1;
+			//c2 = b1;         // right
+			b2 = a + 1;
+			
+			g.indices[ idx     ] = a; // left 
+			g.indices[ idx + 1 ] = b1;
+			g.indices[ idx + 2 ] = c1; 
+			
+			g.indices[ idx + 3 ] = a; // right 
+			g.indices[ idx + 4 ] = b2,
+			g.indices[ idx + 5 ] = b1; // = c2
+			
+			if ( !g.profileMaterial ) g.addGroup( idx, 6, j ); // MultiMaterial support
+			
+			idx += 6;
+			
+		}
+		
+	}
+	
+	if( !g.openEnded ) {
+		
+		g.addGroup( idx, rs * 3, rs ); // MultiMaterial support
+		
+		a = hs1 * rs1;
+		
+		for ( var j = 0; j < rs; j ++ ) {
+			
+			g.indices[ idx     ] = a; 
+			g.indices[ idx + 1 ] = j + 1;
+			g.indices[ idx + 2 ] = j;
+			
+			idx += 3;
+			
+		}
+		
+		g.addGroup( idx, rs * 3, rs + 1 ); // MultiMaterial support
+		
+		a += 1; 
+		
+		for ( var j = rs1 + 1; j > 1; j -- ) {
+			
+			g.indices[ idx     ] = a; 
+			g.indices[ idx + 1 ] = a - j;
+			g.indices[ idx + 2 ] = a - j + 1;
+			
+			idx += 3;
+			
+		}
+		
+	}
+	
+	for ( var i = 0; i < hs1; i ++ ) {
+		
+		i2 = 2 * i; 
+		 
+		xc1 = g.contour[ i2 ];
+		yc1 = g.contour[ i2 + 1 ];
+				
+		if ( i === 0 ) {
+			
+			xc0 = g.contour[ ( hs - 1 ) * 2 ]; // penultimate point
+			yc0 = g.contour[ ( hs - 1 ) * 2 + 1 ];
+			
+		} else {
+					 
+			xc0 = g.contour[ i2 - 2 ]; 	// previous point
+			yc0 = g.contour[ i2 - 1 ];
+			
+		}
+		
+		if ( i === hs ) {
+			
+			xc2 = g.contour[ 2 ];			// second point
+			yc2 = g.contour[ 3 ];
+			
+		} else {
+			 
+			xc2 = g.contour[ i2 + 2 ]; 	// next point
+			yc2 = g.contour[ i2 + 3 ];
+			
+		}	
+		
+		if ( !g.contourClosed ) {
+			
+			if ( i === 0 ) {
+				
+				// direction
+				dx2 = xc2 - xc1;
+				dy2 = yc2 - yc1;
+				
+				// unit vector
+				e2Length = Math.sqrt( dx2 * dx2 + dy2 * dy2 );
+				
+				e2x = dx2 / e2Length;
+				e2y = dy2 / e2Length;
+				
+				// orthogonal
+				ex = e2y;
+				ey = -e2x;
+				
+			}
+			
+			if ( i === hs ) {
+				
+				// direction
+				
+				dx0 = xc1 - xc0;
+				dy0 = yc1 - yc0;
+				
+				// unit vector
+				e0Length = Math.sqrt( dx0 * dx0 + dy0 * dy0 );
+				
+				e0x = dx0 / e0Length;
+				e0y = dy0 / e0Length;
+				
+				// orthogonal
+				ex = e0y;
+				ey = -e0x;
+				
+			}
+			
+			xDiv = 1;
+			bend = 1;
+			
+		}
+		
+		if ( ( i > 0 && i < hs ) || g.contourClosed ) {
+			
+			// directions
+			
+			dx0 = xc0 - xc1;
+			dy0 = yc0 - yc1;
+			
+			dx2 = xc2 - xc1;
+			dy2 = yc2 - yc1;
+			
+			if( Math.abs( ( dy2 / dx2 ) - ( dy0 / dx0 ) ) < epsilon ) { // prevent 0
+				
+				dy0 += epsilon;
+				
+			}
+			
+			if( Math.abs( ( dx2 / dy2 ) - ( dx0 / dy0 ) ) < epsilon ) { // prevent 0
+				
+				dx0 += epsilon;
+				
+			}  
+			
+			// unit vectors
+			
+			e0Length = Math.sqrt( dx0 * dx0 + dy0 * dy0 );
+			
+			e0x = dx0 / e0Length;
+			e0y = dy0 / e0Length;
+			
+			e2Length = Math.sqrt( dx2 * dx2 + dy2 * dy2 );
+			
+			e2x = dx2 / e2Length;
+			e2y = dy2 / e2Length;
+			
+			// direction transformed 
+			
+			ex = e0x + e2x;
+			ey = e0y + e2y;
+			
+			eLength = Math.sqrt( ex * ex + ey * ey );
+			
+			ex = ex / eLength;
+			ey = ey / eLength;
+			
+			phi = Math.acos( e2x * e0x + e2y * e0y ) / 2;
+			
+			bend = Math.sign( dx0 * dy2 - dy0 * dx2 ); // z cross -> curve bending
+			
+			xDiv = Math.sin( phi );
+			
+		}
+		
+		for ( var j = 0; j < rs1; j ++ ) {
+			
+			xSh = g.profileShape[ j * 2 ];
+			
+			x = xc1 + xSh / xDiv * bend * ex; 
+			y = yc1 + xSh / xDiv * bend * ey;
+			z = g.profileShape[ j * 2 + 1 ];	 // ySh
+			
+			vIdx = rs1 * i + j;
+			
+			posIdx = vIdx * 3;
+			
+			g.positions[ posIdx ] = x;
+			g.positions[ posIdx + 1 ] = y;
+			g.positions[ posIdx + 2 ] = z;
+			
+		}
+		
+	}
+	
+	if( !g.openEnded ) {
+		
+		g.positions[ hs1 * rs1 * 3 ] = g.contour[ 0 ];
+		g.positions[ hs1 * rs1 * 3 + 1 ] = g.contour[ 1 ];
+		g.positions[ hs1 * rs1 * 3 + 2 ] = 0;
+		
+		g.positions[ hs1 * rs1 * 3 + 3 ] = g.contour[ hs * 2 ];
+		g.positions[ hs1 * rs1 * 3 + 4 ] = g.contour[ hs * 2 + 1 ];
+		g.positions[ hs1 * rs1 * 3 + 5 ] = 0;
+		
+	}
+	
+	g.computeVertexNormals( );
+	
+}	
+
+exports.createProfiledContourMMgeometry = createProfiledContourMMgeometry;
+exports.buildProfiledContourMMgeometry = buildProfiledContourMMgeometry;
 
 // ..................................... Helper ...................................................
 
